f59e0366d9d4cec8a4333f14fa0081c70c55e9a8
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-24 19:54:09 +0200

    tests: tests for genre, actor, play

--- theatre/models.py
+++ theatre/models.py
@@ -25,7 +25,12 @@ class Play(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        unique=True
+    )
 
     def __str__(self):
         return self.name
--- theatre/pagination.py
+++ theatre/pagination.py
@@ -3,24 +3,24 @@ from rest_framework.pagination import PageNumberPagination
 
 class ActorPagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 100
+    max_page_size = 20
 
 
 class GenrePagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 100
+    max_page_size = 20
 
 
 class PlayPagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 100
+    max_page_size = 20
 
 
 class PerformancePagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 100
+    max_page_size = 20
 
 
 class ReservationPagination(PageNumberPagination):
     page_size = 10
-    max_page_size = 100
+    max_page_size = 20
